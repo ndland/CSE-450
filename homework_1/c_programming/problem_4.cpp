8e17ef5ff346a0d5a86d9e4fa2d4b636ad7c8c8f
Nicholas Land <nland@authen2cate.com>
2016-01-17 13:13:42 -0500

    Fixed problem 4 and 5 source code

---
+++ homework_1/c_programming/problem_4.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+// Prototype so that the main function can call
+// string_duplicator
+char *string_duplicator(char *s);
+
+int main(int argc, char const *argv[]) {
+        char testString[] = "hello";
+        printf("%s\n", string_duplicator(testString));
+}
+
+char *string_duplicator(char *s) {
+        char *cpy = new char;
+        for (int i = 0; i <= strlen(s); i++) {
+                cpy[i] = s[i];
+        }
+        return(cpy);
+}
+g
---
+++ homework_1/c_programming/problem_5.cpp
@@ -0,0 +1,16 @@
+int test(char *s) {
+        int x = 0;
+        unsigned char c; while (*s) {
+                c = *s; while (c) {
+                        if (c & 1) x++;
+                        c = c >>1;
+                }
+                s++;
+        }
+        return x;
+}
+
+int main(int argc, char const *argv[]) {
+        char testString[] = "Hello, World";
+        printf("%d\n", test(testString));
+}
